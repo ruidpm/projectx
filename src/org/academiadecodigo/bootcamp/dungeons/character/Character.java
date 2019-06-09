3f62d1684f65adfd4889f533109e5d67d8231cf1
Ana Pinto <acastanheirapinto@gmail.com>
2019-06-09 20:30:39 +0100

    character && DAMAGE_TYPES

--- src/org/academiadecodigo/bootcamp/dungeons/character/Character.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/Character.java
@@ -9,8 +9,7 @@ public abstract class Character {
     private int magicalResistance;
     private int evasionChance;
     private int criticalChance;
-    private boolean dead;
-    private ReturningAttackValues returningAttackValues;
+    private boolean dead; // If usefull for the game
 
     public Character(int healthPoints, int strength, int intelligence, int physicalResistance, int magicalResistance, int evasionChance, int criticalChance, ReturningAttackValues returningAttackValues){
         this.healthPoints = healthPoints;
@@ -23,13 +22,47 @@ public abstract class Character {
         this.dead = false;
     }
 
-    public void calculateDamage(int damage, DAMAGE_TYPES damageType){
-
-
-
+    public int getHealthPoints() {
+        return healthPoints;
     }
 
-    public void attack(){}
+    public int getEvasionChance() {
+        return evasionChance;
+    }
+
+    public int getCriticalChance() {
+        return criticalChance;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void calculateDamage(int damage, DAMAGE_TYPES damageType){
+
+        switch (damageType) {
+            case MAGICAL:
+                damage -= magicalResistance;
+                intelligence -= damage;
+            break;
+                // Magical types impact intelligence; Physical impact strength;
+                // Magical resistance decrements damage made with Magical type;
+                // Physical resistance decrements damage made with Physical type;
+                // No logic in the choice of values yet;
+            case PHYSICAL:
+                damage -= physicalResistance;
+                strength -= damage;
+            break;
+        }
+        healthPoints = intelligence + strength*2; // physical strength values 2/3 total health points;
+        if (healthPoints <= 0) {
+            dead = true;
+        }
+    }
+
+    public void attack(){
+
+    }
 
 
 
--- src/org/academiadecodigo/bootcamp/dungeons/character/DAMAGE_TYPES.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/DAMAGE_TYPES.java
@@ -2,7 +2,19 @@ package org.academiadecodigo.bootcamp.dungeons.character;
 
 public enum DAMAGE_TYPES {
 
-    PHYSICAL,
-    MAGYCAL
+    PHYSICAL (0,20),
+    MAGICAL (20, 0);
+    // The arguments were just to try different uses with this Enum, if we prefer.
+    // No logic in the choice of the range of values yet;
+
+
+    private int intDamage;
+    private int strDamage;
+
+
+    DAMAGE_TYPES(int intDamage, int strDamage){
+        this.intDamage = intDamage;
+        this.strDamage = strDamage;
+    }
 
 }
