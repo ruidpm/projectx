30f5b560eb8c6884896dd17fa56a642b18f690e0
Rui Magalhães <ruidpm2@gmail.com>
2019-06-09 12:14:13 +0100

    Start implementation of Game.java methods

--- src/org/academiadecodigo/bootcamp/dungeons/Game.java
+++ src/org/academiadecodigo/bootcamp/dungeons/Game.java
@@ -1,21 +1,78 @@
 package org.academiadecodigo.bootcamp.dungeons;
+import org.academiadecodigo.bootcamp.dungeons.character.enemy.EnemyFactory;
 import org.academiadecodigo.bootcamp.dungeons.character.player.PLAYER_SKILLS;
 import org.academiadecodigo.bootcamp.dungeons.character.player.Player;
+import org.academiadecodigo.bootcamp.dungeons.character.enemy.Enemy;
 
 import java.util.LinkedList;
 
 public class Game {
 
     private Player player;
-    private LinkedList<PLAYER_SKILLS> playerSpellList;
+    private LinkedList<PLAYER_SKILLS> playerPossibleSpellsList;
 
 
-    public void init(){}
+    public Game(){
 
-    public void start(){}
+        init();
+    }
 
-    private void battle(){}
 
-    private void eliteBattle(){}
+    public void init(){
 
+        player = new Player ();
+
+        createPlayerPossibleSkillsList();        //store all possible player skills in a list
+    }
+
+
+    public void start(){
+
+        while (player.getHealthPoints >= 0){
+
+            if ( battle(EnemyFactory.createEnemy())) { //battle returns true if player defeats the enemy
+                                                       //or false if he flees
+                generateLoot();                         //he only gets a chance for loot if he doesn't flee
+            }
+
+            if ("player chooses to rest"){
+
+                if (!player.rest()){ //if rest is not successful generate elite battle
+                    battle(EnemyFactory.createEliteEnemy()); //monster with random skills and higher stats
+                }
+            }
+        }
+    }
+
+
+    private boolean battle(Enemy enemy){
+
+        while (player.getHealthPoints > 0 || enemy.getHealthPoints > 0){
+
+            player.takeAction();
+
+            if (enemy.getHealthPoints() > 0) {
+                enemy.attack();
+            }
+        }
+
+        if (player.getHealthPoints <= 0){
+            gameOver();
+        }
+
+        return true;
+    }
+
+
+    private void createPlayerPossibleSkillsList(){
+
+        for (PLAYER_SKILLS skill : PLAYER_SKILLS.values()) {
+            playerPossibleSpellsList.add(skill);
+        }
+    }
+
+
+    private void generateLoot(){ }
+
+    private void gameOver(){}
 }
