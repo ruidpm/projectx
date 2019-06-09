2a61f424c1dbb43b2282e5be3738bf0ad004dba4
thiagoltavares <thiago_maha@hotmail.com>
2019-06-09 12:10:59 +0100

    Character Constructor created

--- src/org/academiadecodigo/bootcamp/dungeons/character/enemy/Enemy.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/enemy/Enemy.java
@@ -3,4 +3,10 @@ package org.academiadecodigo.bootcamp.dungeons.character.enemy;
 import org.academiadecodigo.bootcamp.dungeons.character.Character;
 
 public class Enemy extends Character {
+
+    public Enemy(int healthPoints, int strength, int intelligence, int physicalResistance, int magicalResistance, int evasionChance, int criticalChance){
+        super(healthPoints, strength, intelligence, physicalResistance, magicalResistance, evasionChance, criticalChance);
+    }
+
+    //todo
 }
