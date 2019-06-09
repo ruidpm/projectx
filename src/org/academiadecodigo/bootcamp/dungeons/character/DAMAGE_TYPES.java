d232bebdb5c9327730d73098ac0bc94901291c1a
Ana Pinto <acastanheirapinto@gmail.com>
2019-06-09 22:28:03 +0100

    character & Damage Types

--- src/org/academiadecodigo/bootcamp/dungeons/character/DAMAGE_TYPES.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/DAMAGE_TYPES.java
@@ -4,9 +4,9 @@ public enum DAMAGE_TYPES {
 
     PHYSICAL (0,20),
     MAGICAL (20, 0);
+
     // The arguments were just to try different uses with this Enum, if we prefer.
     // No logic in the choice of the range of values yet;
-
 
     private int intDamage;
     private int strDamage;
