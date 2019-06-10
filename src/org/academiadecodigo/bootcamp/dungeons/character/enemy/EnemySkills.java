e894b069667bf536d8e524f81fbbca3590371d8f
Thiago Tavares <thiagodevtavares@gmail.com>
2019-06-10 17:04:50 +0100

    Enum was wrong, so now its ok

--- src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemySkills.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemySkills.java
@@ -1,18 +1,18 @@
 package org.academiadecodigo.bootcamp.dungeons.character.enemy;
 
-import org.academiadecodigo.bootcamp.dungeons.character.DAMAGE_TYPES;
+import org.academiadecodigo.bootcamp.dungeons.character.DamageTypes;
 
 public enum EnemySkills {
-    CHARGE(20, DAMAGE_TYPES.PHYSICAL),
-    FIRE_BOMB(30, DAMAGE_TYPES.MAGICAL),
-    DOUBLE_ATTACK(40, DAMAGE_TYPES.PHYSICAL),
-    ICYCLE(50, DAMAGE_TYPES.MAGICAL);
+    CHARGE(20, DamageTypes.PHYSICAL),
+    FIRE_BOMB(30, DamageTypes.MAGICAL),
+    DOUBLE_ATTACK(40, DamageTypes.PHYSICAL),
+    ICYCLE(50, DamageTypes.MAGICAL);
 
     int spellDamage;
-    DAMAGE_TYPES damage_types;
+    DamageTypes damage_types;
 
 
-    EnemySkills(int spellDamage, DAMAGE_TYPES damage_types) {
+    EnemySkills(int spellDamage, DamageTypes damage_types) {
         this.spellDamage = spellDamage;
         this.damage_types = damage_types;
     }
