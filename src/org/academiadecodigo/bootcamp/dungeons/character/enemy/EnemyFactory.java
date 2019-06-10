85140763d260725d3de1f9e8b38ce9a013a33967
Thiago Tavares <thiagodevtavares@gmail.com>
2019-06-10 17:29:41 +0100

    Hunter and minotaur chance is reduced

--- src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemyFactory.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemyFactory.java
@@ -8,28 +8,28 @@ public class EnemyFactory {
         Enemy enemy;
 
 
-        if (Randomizer.getPercentage() > 95) {
+        if (Randomizer.getPercentage() >= 98) {
             enemy = new Enemy(EnemyTypes.HUNTER);
             return enemy;
         }
 
-        if (Randomizer.getPercentage() > 90) {
+        if (Randomizer.getPercentage() >= 95) {
             enemy = new Enemy(EnemyTypes.MINOTAUR);
             return enemy;
         }
-        if (Randomizer.getPercentage() > 75) {
+        if (Randomizer.getPercentage() >= 75) {
             enemy = new Enemy(EnemyTypes.SKELETON);
             return enemy;
         }
-        if (Randomizer.getPercentage() > 60) {
+        if (Randomizer.getPercentage() >= 60) {
             enemy = new Enemy(EnemyTypes.ORC);
             return enemy;
         }
-        if (Randomizer.getPercentage() > 40) {
+        if (Randomizer.getPercentage() >= 40) {
             enemy = new Enemy(EnemyTypes.GOBLIN);
             return enemy;
         }
-        if (Randomizer.getPercentage() > 20) {
+        if (Randomizer.getPercentage() >= 20) {
             enemy = new Enemy(EnemyTypes.TROLL);
             return enemy;
         }
