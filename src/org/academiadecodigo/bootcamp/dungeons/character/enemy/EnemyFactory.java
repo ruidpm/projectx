fd455f17ed1c1528e1bb873b160e0603c829ce2e
Thiago Tavares <thiagodevtavares@gmail.com>
2019-06-10 17:13:35 +0100

    Removing new instance from createEnemy method

--- src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemyFactory.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/enemy/EnemyFactory.java
@@ -5,7 +5,7 @@ import org.academiadecodigo.bootcamp.dungeons.Randomizer;
 public class EnemyFactory {
 
     public static Enemy createEnemy() {
-        Enemy enemy = new Enemy(EnemyTypes.RAT);
+        Enemy enemy;
 
 
         if (Randomizer.getPercentage() > 95) {
@@ -33,8 +33,8 @@ public class EnemyFactory {
             enemy = new Enemy(EnemyTypes.TROLL);
             return enemy;
         }
-      
-        return enemy;
+
+        return enemy = new Enemy(EnemyTypes.RAT);
 
     }
 
