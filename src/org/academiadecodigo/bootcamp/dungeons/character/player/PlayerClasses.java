9891757885834a9da1fc82000b0ae4e7549b50b6
Pedro Albuquerque <pedromsalbur@gmail.com>
2019-06-11 23:51:08 +0100

    added some player skills

--- src/org/academiadecodigo/bootcamp/dungeons/character/player/PlayerClasses.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/player/PlayerClasses.java
@@ -4,11 +4,11 @@ import org.academiadecodigo.bootcamp.dungeons.character.player.items.WeaponTypes
 
 public enum PlayerClasses {
     WIZARD ("Izadarian",250,5,20,1,10,
-            5,10, 50, WeaponTypes.STAFF),
+            5,10, 50, WeaponTypes.STAFF, PlayerSkills.UNHOLYHEAL),
     WARRIOR("Edoqor",300,15,5,10,1,
-            5,5, 20, WeaponTypes.IRONAXE),
+            5,5, 20, WeaponTypes.IRONAXE, PlayerSkills.SPINNINGRAGE),
     NINJA("Ogorim",250,10,15,5,5,
-            15,15, 30, WeaponTypes.SHURIKEN);
+            15,15, 30, WeaponTypes.SHURIKEN, PlayerSkills.SPEARSTEB);
 
     private String name;
     private int healthPoints;
@@ -20,9 +20,10 @@ public enum PlayerClasses {
     private int criticalChance;
     private int manaPoints;
     private WeaponTypes weapon;
+    private PlayerSkills playerSkills;
 
     PlayerClasses(String name, int healthPoints, int strength, int intelligence, int physicalResistance, int magicalResistance,
-                  int evasionChance, int criticalChance, int manaPoints, WeaponTypes weapon) {
+                  int evasionChance, int criticalChance, int manaPoints, WeaponTypes weapon, PlayerSkills playerSkills) {
         this.name=name;
         this.healthPoints=healthPoints;
         this.strength=strength;
@@ -33,6 +34,7 @@ public enum PlayerClasses {
         this.criticalChance=criticalChance;
         this.manaPoints=manaPoints;
         this.weapon=weapon;
+        this.playerSkills=playerSkills;
     }
 
     public String getName() {
--- src/org/academiadecodigo/bootcamp/dungeons/character/player/PlayerSkills.java
+++ src/org/academiadecodigo/bootcamp/dungeons/character/player/PlayerSkills.java
@@ -1,10 +1,32 @@
 package org.academiadecodigo.bootcamp.dungeons.character.player;
 
+import org.academiadecodigo.bootcamp.dungeons.character.DamageTypes;
+
 public enum PlayerSkills {
-    DOUBLEATTACK,
-    YELL,
-    SPEARSTEB,
-    DARKSOUL,
-    INNERDEAMON,
+    DOUBLEATTACK (25 ,DamageTypes.PHYSICAL),
+    YELL (20, DamageTypes.PHYSICAL),
+    SPEARSTEB (25, DamageTypes.PHYSICAL),
+    DARKSOUL (35, DamageTypes.MAGICAL),
+    INNERDEAMON (40, DamageTypes.MAGICAL),
+    DRAGONRAGE (30, DamageTypes.PHYSICAL),
+    HYPNOSE (15, DamageTypes.MAGICAL),
+    UNHOLYHEAL (35, DamageTypes.MAGICAL),
+    SPINNINGRAGE (35, DamageTypes.PHYSICAL),
+    SIDESMASH (30, DamageTypes.PHYSICAL),
+    PSYCHODRAIN (35, DamageTypes.MAGICAL),
+    ACIDNEBULA (25, DamageTypes.PHYSICAL),
+    TRAPSOUL (20, DamageTypes.MAGICAL),
+    INFINITELOOP (35, DamageTypes.MAGICAL),
+    CRUMBLEFIST (35, DamageTypes.PHYSICAL);
+
+    
+    int skillDamage;
+    DamageTypes damageTypes;
+
+    PlayerSkills(int spellDamage, DamageTypes damage_types) {
+        this.skillDamage = spellDamage;
+        this.damageTypes = damage_types;
+    }
+
 
 }
