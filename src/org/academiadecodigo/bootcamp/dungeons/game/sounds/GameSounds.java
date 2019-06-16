c5f9fa19fd3228792ed10d3c065be1907b854b51
Ana Pinto <acastanheirapinto@gmail.com>
2019-06-16 12:49:25 +0100

    sounds organization

--- src/org/academiadecodigo/bootcamp/dungeons/game/sounds/GameSounds.java
+++ src/org/academiadecodigo/bootcamp/dungeons/game/sounds/GameSounds.java
@@ -4,30 +4,30 @@ package org.academiadecodigo.bootcamp.dungeons.game.sounds;
 public class GameSounds {
 
 
-    public static Sound gameMusic = new Sound("/resources/Background/Forward-Assault.wav");
+    public static Sound gameMusic = new Sound("/resources/Background/Forward-Assault_Loop.wav");
 
-    public static Sound wrongMenuChoice = new Sound("/resources/Creepy.wav");
+    public static Sound wrongMenuChoice = new Sound("/resources/wrong-choice.wav");
 
-    //public static Sound gameStart = new Sound("/resources/Background/.wav");
+    public static Sound gameStart = new Sound("/resources/game-start-bell/.wav");
 
-    public static Sound enemyAppears = new Sound("/resources/black.wav");
+    public static Sound enemyAppears = new Sound("/resources/enemy-appears.wav");
 
-    public static Sound enemyAttack = new Sound("/resources/Fireball+2.wav");
+    public static Sound enemyAttack = new Sound("/resources/enemy-attack.wav");
 
-    public static Sound drinkPotion = new Sound("/resources/Slurp.wav");
+    public static Sound drinkPotion = new Sound("/resources/slurp.wav");
 
-    //public static Sound playerSkillAttack = new Sound("/resources/Into-Battle.wav");
+    //public static Sound playerSkillAttack = new Sound("/resources/.wav");
 
-    //public static Sound playerWeaponAttack = new Sound("/resources/Into-Battle.wav");
+    public static Sound playerWeaponAttack = new Sound("/resources/attack-weapon.wav");
 
-    public static Sound levelUp = new Sound("/resources/chipquest.wav");
+    public static Sound levelUp = new Sound("/resources/cheering.wav");
 
-    public static Sound restSound = new Sound("/resources/Yawn.wav");
+    public static Sound restSound = new Sound("/resources/rest.wav");
 
-    //public static Sound ambushSound = new Sound("/resources/Into-Battle.wav");
+    public static Sound ambushSound = new Sound("/resources/enemyWins.wav");
 
-    public static Sound victorySound = new Sound("/resources/Ta-Da.wav");
+    //public static Sound victorySound = new Sound("/resources/Ta-Da.wav");
 
-    //public static Sound gameOverSound = new Sound("/resources/Into-Battle.wav");
+    public static Sound enemyWins = new Sound("/resources/enemyWins.wav");
 
 }
